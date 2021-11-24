313d2a8bbd7c3fa3287d3c88613a8523a7b5474a
cbedroid <cbedroid1614@gmail.com>
2021-11-23 20:05:13 -0500

    Added restframework throttle config for API

--- django/photoshare/settings.py
+++ django/photoshare/settings.py
@@ -150,6 +150,13 @@ REST_FRAMEWORK = {
     "DEFAULT_PARSER_CLASSES": ("rest_framework.parsers.FormParser", "rest_framework.parsers.MultiPartParser"),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 25,
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "120/min",
+    },
+    "NUM_PROXIES": 1,
 }
 
 # Internationalization
