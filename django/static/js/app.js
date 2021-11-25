58c7ec8201cb1227549541bcfab3b84d9274376a
cbedroid <cbedroid1614@gmail.com>
2021-11-25 08:20:04 -0500

    Updated gallery templates layouts and styles

--- django/static/js/app.js
+++ django/static/js/app.js
@@ -15,7 +15,6 @@ $(document).ready(function () {
       Magnific Popup
   ------------------------------------*/
   $("#gallery-photo-collections .photo__card").each(function () {
-    console.log("Gallery Photo", this);
     $(this)
       .find(".share_popup")
       .magnificPopup({
@@ -65,49 +64,6 @@ $(document).ready(function () {
   $("#aside_checkbox").on("change", function () {
     toggleCheckBoxMenu();
   });
-
-  /**
-   * CUSTOM IMAGE UPLOAD FORM SCRIPT
-   **/
-  (function () {
-    const photo_form = $(".photoform__card");
-    const form_inputs = $(photo_form).find("input[type=file]");
-
-    if (photo_form) {
-      const image_labels = $(".photoform__card").find("label");
-      // Attach attr "for" to the custom input
-      $(form_inputs).each((i, c) => {
-        $(image_labels[i]).attr("for", $(c).attr("id"));
-      });
-
-      $(form_inputs).on("change", function () {
-        // Set image preview and change label to filename name
-        const image_file = this.files[0];
-
-        // set preview image
-        const filereader = new FileReader();
-        const form_image = $(photo_form).find(".form-image");
-        if (form_image && image_file) {
-          filereader.readAsDataURL(image_file);
-          // wait until image is loaded
-          filereader.onloadend = (e) => {
-            form_image[0].src = e.target.result;
-          };
-        }
-        // Change form label name to the upload file name
-        const custom_label = $(this).siblings("label").find(".image__label");
-        $(custom_label)
-          .addClass("font-semibold")
-          .attr({
-            title: image_file.name,
-          })
-          .text(image_file.name);
-      });
-    }
-  })();
-  /**
-   * CUSTOM IMAGE UPLOAD FORM SCRIPT END
-   **/
 
   /* --------------------------------
       Window and Main Events
