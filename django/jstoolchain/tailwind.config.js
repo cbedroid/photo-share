d6f649c3a41180010f184a84efa84cdbaa2e9330
cbedroid <cbedroid1614@gmail.com>
2021-11-28 08:09:19 -0500

    Updated Tailed config variant - Added fill theme

--- django/jstoolchain/tailwind.config.js
+++ django/jstoolchain/tailwind.config.js
@@ -3,10 +3,22 @@ module.exports = {
   purge: ["../templates/**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fill: theme => ({
+
+        'red': theme('colors.red.500'),
+
+        'green': theme('colors.green.500'),
+
+        'blue': theme('colors.blue.500'),
+      })
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      fill: ['hover', 'focus'],
+
+    },
   },
   plugins: [require("@tailwindcss/forms"), require("daisyui")],
 };
