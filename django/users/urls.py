35eb6517a2ac652bc290bc01ab42c140e0a087a3
cbedroid <cbedroid1614@gmail.com>
2021-11-28 08:31:23 -0500

    Added user account :update view and deletion view

--- django/users/urls.py
+++ django/users/urls.py
@@ -1,2 +1,11 @@
+from django.urls import re_path
+
+from . import views
+
 app_name = "user"
-urlpatterns = []
+# fmt: off
+urlpatterns = [
+    re_path(r"^profile/update/(?P<slug>.*)/$", views.UserUpdateView.as_view(), name="user-update"),
+    re_path(r"^account/delete/(?P<slug>.*)/$", views.UserAccountDeleteView.as_view(), name="user-delete"),
+]
+# fmt: on
