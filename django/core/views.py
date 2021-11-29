61d163c362fdf12e29d4bf130ef62fd2416d98d7
cbedroid <cbedroid1614@gmail.com>
2021-11-29 13:21:19 -0500

    Remodeling Index View

--- django/core/views.py
+++ django/core/views.py
@@ -1,28 +1,13 @@
-from django.db.models import Count, Q, Sum
-from django.views.generic import ListView
-from gallery.models import Category, Gallery
+from django.shortcuts import redirect
+from django.views.generic import TemplateView
 
 
-class HomeListView(ListView):
-    model = Gallery
+class HomeListView(TemplateView):
     template_name = "core/index.html"
 
-    def get_queryset(self):
+    def get(self, *args, **kwargs):
+        # NOTE: Temporary redirect  logged in users to galleylist
+        #       until index login layouts are completed
         if self.request.user.is_authenticated:
-            qs = Gallery.objects.filter(Q(public=True) | Q(user=self.request.user))
-        qs = Gallery.objects.filter(public=True)
-
-        # Preform lookup searches in all Gallery's related field
-        search = self.request.GET.get("q")
-        if search:
-            return Gallery.objects.query_search(search, qs)
-
-        # show only top 20 gallery with the most views
-        return qs.annotate(photo_views=Sum("photos__views")).order_by("-photo_views")[:20]
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # show top 20 trending category
-        context["by_search"] = self.request.GET.get("q", False)
-        context["top_category"] = Category.objects.alias(c=Count("gallery")).order_by("-c")[:20]
-        return context
+            return redirect("gallery:gallery-list")
+        return super().get(*args, **kwargs)
