7b02919134fe832c592ba2cd59b9ab1316b9426f
cbedroid <cbedroid1614@gmail.com>
2021-11-29 13:19:05 -0500

    Updated tailwind config

--- django/jstoolchain/tailwind.config.js
+++ django/jstoolchain/tailwind.config.js
@@ -1,22 +1,37 @@
+
+const plugin = require("tailwindcss/plugin");
+
+// const siblingCheckedPlugin = plugin(function ({ addVariant, e }) {
+//   addVariant("sibling-checked", ({ container }) => {
+//     container.walkRules((rule) => {
+//       rule.selector = `:checked + .sibling-checked\\:${rule.selector.slice(1)}`;
+//     });
+//   });
+// });
+
 module.exports = {
   mode: "jit",
   purge: ["../templates/**/*.html"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
       fill: theme => ({
-
         'red': theme('colors.red.500'),
-
         'green': theme('colors.green.500'),
-
+        'yellow': theme('colors.yellow.300'),
+        'dark-yellow': theme('colors.yellow.500'),
+        'gray': theme('colors.green.300'),
         'blue': theme('colors.blue.500'),
-      })
+      }),
+      colors: {
+        site: '#FF6E0A'
+      }
     },
   },
   variants: {
     extend: {
-      fill: ['hover', 'focus'],
+
+      fill: ['hover', 'focus']
 
     },
   },
