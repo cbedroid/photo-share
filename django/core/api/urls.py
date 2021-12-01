32309023b7d550e479b6cac2e50d6a865209f5ec
cbedroid <cbedroid1614@gmail.com>
2021-11-30 19:53:49 -0500

    Fixed restframework api urls

--- django/core/api/urls.py
+++ django/core/api/urls.py
@@ -1,16 +1,12 @@
-from django.urls import include, path
 from rest_framework import routers
 
 from . import views
 
+app_name = "api"
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r"user", views.UserViewSet)
 router.register(r"gallery", views.GalleryViewSet)
 router.register(r"photo", views.PhotoViewSet)
-app_name = "api"
 
-urlpatterns = [
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-    path("", include(router.urls)),
-]
+urlpatterns = router.urls
