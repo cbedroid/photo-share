cc5447c4187a04bf2a676db08da180f778baacd8
cbedroid <cbedroid1614@gmail.com>
2022-07-12 02:21:42 -0400

    Updated staticfiles

---
+++ django/static/js/app.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+  /*
+   * Alert Message
+    */
+  (function handleAlertMessage() {
+    const message_alert = document.getElementById("alert_message");
+    $(message_alert).delay(5000).fadeOut(1500);
+    $(".close").bind('click', function () {
+      $(this).parent().remove();
+    });
+  })();
+
+  /**
+   * Magnific Popup
+   *
+   */
+  (function handleMagnificPopup() {
+    $("body").each(function () {
+      $(this).
+        find(".share_popup")
+        .magnificPopup({
+          items: [
+            {
+              src: $(this).find(".social-share").first(),
+              type: "inline",
+            },
+          ],
+          closeOnContentClick: true,
+          closeBtnInside: false,
+        });
+    });
+  })();
+
+
+  /**
+   * Aside CheckBox Events
+   */
+  (function handleSideBar() {
+    $("#aside_checkbox").bind("change", function () {
+      // swap aside toggler from bars to times and vice-versa.
+      $('svg.toggler').toggleClass('hidden');
+
+    });
+  })();
+
+}); //ready
