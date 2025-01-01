3479db649e0b5c38233f6d345c4a887336dc8348
cbedroid <cbedroid1614@gmail.com>
2025-01-01 17:10:34 -0500

    Add abstract model to keep created and updated fields consistent throughout app

--- django/core/models.py
+++ django/core/models.py
@@ -4,6 +4,16 @@ from django.utils.translation import gettext_lazy as _
 from simple_history import register
 
 
+class PhotoShareBaseModel(models.Model):
+    """Base Model for all models."""
+
+    created = models.DateTimeField(auto_now_add=True, help_text="An ISO 8601 date time of when this was created")
+    updated = models.DateTimeField(auto_now=True, help_text="An ISO 8601 date time of when this was last updated")
+
+    class Meta:
+        abstract = True
+
+
 class FAQTopic(models.Model):
     name = models.CharField(max_length=40)
     description = models.TextField(max_length=200)
@@ -18,10 +28,14 @@ class FAQ(models.Model):
     topic = models.ForeignKey(FAQTopic, null=True, on_delete=DO_NOTHING)
 
     question = models.CharField(
-        max_length=150, blank=False, help_text=_("Frequently asked question (150 max char) required")
+        max_length=150,
+        blank=False,
+        help_text=_("Frequently asked question (150 max char) required"),
     )
     answer = models.TextField(
-        max_length=500, blank=False, help_text=_("Frequently asked question's answer (500 max char) required")
+        max_length=500,
+        blank=False,
+        help_text=_("Frequently asked question's answer (500 max char) required"),
     )
 
     def __str__(self):
