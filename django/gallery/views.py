194cb14bc19f9b9056cce2a5d88569d6f5a4c39a
cbedroid <cbedroid1614@gmail.com>
2025-01-01 18:09:28 -0500

    Refactor Gallery, Photos and User views.

--- django/gallery/views.py
+++ django/gallery/views.py
@@ -1,9 +1,11 @@
 import json
+from typing import TYPE_CHECKING, Optional, Union
 
+from core.helpers import is_ajax_request
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.db.models import Count, Q, Sum
-from django.http.response import JsonResponse
+from django.db.models import Count, Q, QuerySet
+from django.http.response import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse, reverse_lazy
 from django.views.generic import (
@@ -18,6 +20,9 @@ from django.views.generic.list import MultipleObjectMixin
 from .mixins import GalleryFormMixin, UserAccessPermissionMixin
 from .models import Category, Gallery, Photo
 
+if TYPE_CHECKING is True:
+    from django.http import HttpRequest
+
 
 class GalleryDetailView(DetailView, MultipleObjectMixin):
     model = Gallery
@@ -26,32 +31,34 @@ class GalleryDetailView(DetailView, MultipleObjectMixin):
     paginate_by = 25
 
     # Throws 404 if gallery is private
-    def get_queryset(self):
+    def get_queryset(self) -> QuerySet[Gallery]:
+        qs = super().get_queryset()
         user = self.request.user
+        filter_condition = Q(public=True)
         if user.is_authenticated:
-            return Gallery.objects.filter(Q(public=True) | Q(user=self.request.user))
-        else:
-            return Gallery.objects.filter(public=True)
+            filter_condition |= Q(user=user)
+        return qs.filter(filter_condition)
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs) -> dict:
+        current_user = self.request.user
         obj = self.get_object()
-        object_list = obj.photos.order_by("-pk")
-        context = super().get_context_data(object_list=object_list, **kwargs)
-        related_gallery = self.object.category.gallery
-        user = self.request.user
-        if related_gallery.exists():
+        related_gallery = obj.category.galleries
+        photo_qs = obj.photos.all()
+
+        context = super().get_context_data(object_list=photo_qs, **kwargs)
+        if related_gallery:
             # Filter galleries by owner or its public state
             # Only display galleries if user is the owner or gallery is public
-            if user.is_authenticated:
+            if current_user.is_authenticated:
                 related_gallery = related_gallery.filter(
                     Q(public=True) | Q(user=self.request.user),
-                ).exclude(pk=self.object.pk)
+                ).exclude(id=obj.id)
             else:
-                related_gallery = related_gallery.filter(public=True).exclude(pk=self.object.pk)
+                related_gallery = related_gallery.filter(public=True).exclude(id=obj.id)
 
         context["related_gallery"] = related_gallery[:20]
-        context["is_user"] = obj.user == self.request.user
-        context["cover_photo"] = obj.photos.filter(is_cover=True).last()
+        context["is_user"] = obj.user == current_user
+        context["cover_photo"] = obj.cover_photo
         return context
 
 
@@ -60,24 +67,28 @@ class GalleryListView(ListView):
     template_name = "gallery/gallery_list.html"
     paginate_by = 25
 
-    def get_queryset(self):
-        if self.request.user.is_authenticated:
-            qs = Gallery.objects.filter(Q(public=True) | Q(user=self.request.user))
+    def get_queryset(self) -> QuerySet[Gallery]:
+        qs = super().get_queryset()
+        user = self.request.user
+        if user.is_authenticated:
+            qs = qs.filter(Q(public=True) | Q(user=self.request.user))
         else:
-            qs = Gallery.objects.filter(public=True)
+            qs = qs.with_public_photos()
 
         search = self.request.GET.get("q")
         if search:
-            return Gallery.objects.query_search(search, qs)
+            return qs.query_search(search)
 
         # TODO order by datetime range  - one to two week newest
-        return qs.annotate(photo_views=Sum("photos__views")).order_by("?", "-photo_views")
+        return qs.with_total_views().order_by("?", "-total_views")
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs) -> dict:
         context = super().get_context_data(**kwargs)
         # Show top 20 trending category
         context["by_search"] = self.request.GET.get("q", False)
-        context["top_category"] = Category.objects.alias(c=Count("gallery")).order_by("-c")[:20]
+        context["top_category"] = Category.objects.alias(total_galleries=Count("galleries")).order_by(
+            "-total_galleries"
+        )[:20]
         return context
 
 
@@ -94,18 +105,18 @@ class GalleryUpdateView(GalleryFormMixin, UserPassesTestMixin, UpdateView):
     template_name = "gallery/gallery_form.html"
     object = None
 
-    def test_func(self):
+    def test_func(self) -> bool:
         """Test whether the Gallery belongs to the current user"""
         return self.request.user == self.get_object().user
 
 
 class GalleryDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Gallery
-    template_name = "galle(ry/gallery_confirm_delete.html"
+    template_name = "gallery/snippets/modals/gallery_confirm_delete.html"
     login_url = reverse_lazy("account_login")
     success_url = reverse_lazy("core:index")
 
-    def test_func(self):
+    def test_func(self) -> bool:
         """Test whether the Gallery belongs to the current user"""
         return self.request.user == self.get_object().user
 
@@ -118,56 +129,52 @@ class PhotoDetailView(UserAccessPermissionMixin, DetailView):
     slug_url_kwarg = "slug"
 
     # Throws 404 if photo gallery is private
-    def get_queryset(self):
+    def get_queryset(self) -> QuerySet[Photo]:
         user = self.request.user
+        qs = super().get_queryset()
         if user.is_authenticated:
-            return Photo.objects.filter(Q(gallery__public=True) | Q(gallery__user=self.request.user))
+            qs = qs.filter(Q(gallery__public=True) | Q(gallery__user=self.request.user))
         else:
-            return Photo.objects.filter(gallery__public=True)
+            qs = qs.with_public_photos()
+        return qs.all()
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs) -> dict:
         context = super().get_context_data(**kwargs)
-        obj = self.get_object()
+        obj: Photo = self.get_object()
         context["is_user"] = obj.gallery.user == self.request.user
         context["galleries"] = Gallery.objects.filter(user=obj.gallery.user).exclude(pk=obj.gallery.pk)
         return context
 
-    def get(self, *args, **kwargs):
-        self.object = self.get_object()
-        self.object.views += 1
-        self.object.save()
-        context = self.get_context_data()
-        return self.render_to_response(context)
+    def get(self, *args, **kwargs) -> HttpResponse:
+        obj = self.get_object()
+        obj.views += 1
+        obj.save()
+        return super().get(*args, **kwargs)
 
 
 class PhotoDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Photo
     template_name = "gallery/photo_confirm_delete.html"
 
-    def test_func(self):
+    def test_func(self) -> bool:
         """Test whether the Photo belongs to the current user"""
         user = self.get_object().gallery.user
         return user == self.request.user
 
-    def get_success_url(self, *args, **kwargs):
+    def get_success_url(self, *args, **kwargs) -> str:
         gallery = self.object.gallery
         if gallery.photos.count() > 1:
             return gallery.get_absolute_url()
         return reverse("core:index")
 
-    def post(self, *args, **kwargs):
+    def post(self, *args, **kwargs) -> HttpResponse:
         response = super().post(*args, **kwargs)
         gallery = self.object.gallery
         if gallery.photos.count() == 0:
             gallery.delete()
             messages.warning(
                 self.request,
-                "".join(
-                    (
-                        f'Your gallery "{gallery}" was deleted,',
-                        " because it does not contain any photos",
-                    )
-                ),
+                f"Your gallery '{gallery}' was deleted, " "because it does not contain any photos",
             )
         return response
 
@@ -181,8 +188,8 @@ class PhotoUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         user = self.get_object().gallery.user
         return user == self.request.user
 
-    def post(self, *args, **kwargs):
-        if self.request.is_ajax():
+    def post(self, request: "HttpRequest", *args, **kwargs) -> HttpResponse:
+        if is_ajax_request(request):
             response = {"status": False}
             obj = self.get_object()
             data = json.loads(self.request.body)
@@ -190,34 +197,38 @@ class PhotoUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
             obj.save()
             response["status"] = True
             return JsonResponse(response)
-        super().post(*args, **kwargs)
+        return super().post(*args, **kwargs)
 
 
-def photo_cover_update(request, pk=None):
+def photo_cover_update(request: "HttpRequest", pk: Optional[int] = None) -> Union[JsonResponse, HttpResponseRedirect]:
+    """Update the cover photo of a gallery"""
     obj = get_object_or_404(Photo, pk=pk)
     owner = obj.gallery.user
 
     if request.user == owner and request.method == "POST":
-        if request.is_ajax():
-            response = {"status": False}
+        if is_ajax_request(request):
             data = json.loads(request.body)
             obj.is_cover = data.get("cover")
-            obj.save()
-            response["status"] = True
+            obj.save(update_fields=("is_cover", "updated"))
+
+            response = {"status": True}
             return JsonResponse(response)
     return redirect(".")
 
 
-def photo_transfer(request, pk=None):
-    data = dict(request.POST)
+def photo_transfer(request: "HttpRequest", pk: Optional[int] = None) -> HttpResponseRedirect:
+    """Transfer a photo to another gallery"""
 
+    new_gallery_id = request.POST["gallery"][0]
     obj = Photo.objects.get(pk=pk)
-    gallery = Gallery.objects.get(pk=data["gallery"][0])
+    gallery = get_object_or_404(Gallery, id=new_gallery_id)
+    gallery_detail_url = gallery.get_absolute_url()
+
+    # Assign photo to a new gallery
     obj.gallery = gallery
-    obj.save()
-    return redirect(obj.gallery.get_absolute_url())  # core:index")
+    obj.save(update_fields=("gallery", "updated"))
+    return redirect(gallery_detail_url)
 
 
 class CategoryDetailView(DetailView):
     slug_url_kwarg = "slug"
-    pass
--- django/users/views.py
+++ django/users/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.db.models import QuerySet
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic import UpdateView
 from django.views.generic.edit import DeleteView
@@ -38,23 +40,26 @@ class UserAccountDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView)
     template_name = "users/user_confirm_delete.html"
     success_url = reverse_lazy("core:index")
 
-    def test_func(self):
+    def test_func(self) -> bool:
         return self.request.user == self.get_object()
 
-    def get_context_data(self, **kwargs):
+    def get_queryset(self) -> QuerySet[User]:
+        qs = super().get_queryset()
+        return qs.prefetch_related("socialaccount_set")
+
+    def get_context_data(self, **kwargs) -> dict:
         context = super().get_context_data(**kwargs)
-        obj = self.get_object()
-        self.object = self.get_object()
+        obj = self.object
         if self.request.method == "GET":
             context["form"] = self.form_class(instance=obj)
         else:
             context["form"] = self.form_class(instance=obj, data=self.request.POST)
         return context
 
-    def form_valid(self, form):
+    def form_valid(self, form: AccountDeleteForm) -> HttpResponseRedirect:
         context = self.get_context_data()
         if form.has_changed and form.is_valid():
-            # send an email to user
+            # send email to user
             # delete user account
             form.save(commit=True)
             return super().form_valid(form)
